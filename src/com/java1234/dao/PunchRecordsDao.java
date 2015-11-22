f1d2cd1416775c6e39334c93edfe8a1f12b4340b
wuxingliang <wuxingliang@192.168.1.101>
2015-11-22 14:36:14 +0800

    方法实现

---
+++ src/com/java1234/dao/PunchRecordsDao.java
@@ -0,0 +1,7 @@
+package com.java1234.dao;
+
+import com.java1234.entity.PunchRecords;
+
+public interface PunchRecordsDao {
+	public int add(PunchRecords punchRecords);
+}
---
+++ src/com/java1234/service/PunchRecordsService.java
@@ -0,0 +1,8 @@
+package com.java1234.service;
+
+import com.java1234.entity.PunchRecords;
+
+public interface PunchRecordsService {
+
+	public int add(PunchRecords punchRecords);
+}
